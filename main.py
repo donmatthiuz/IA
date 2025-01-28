07b604a86d9e44ba4330836563faf5b64b4ba92c
cor22982 <cor22982@uvg.edu.gt>
2025-01-27 22:57:55 -0600

    feat: COmpletado regresion

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from regresion_logistica import Logistica
 from validacion import f1_score
+from matplotlib.colors import ListedColormap as Colors
 
 
 #=================PARTE 1 ANALISIS Y LIMPIEZA DATASET========================
@@ -111,10 +112,30 @@ epocas = 7000
 regresion = Logistica(learning_rate=learning_rate, epocas=epocas)
 
 y_predichas_mimodelo = regresion.algoritmo_sin_librerias(X=X_train, Y=y_train, w=w, b=b, X_test=X_test)
-y_predichas_modelo_sklearn = regresion.algoritmo_con_librerias(X_train=X_train, Y_train=y_train, X_test=X_test)
+y_predichas_modelo_sklearn= regresion.algoritmo_con_librerias(X_train=X_train, Y_train=y_train, X_test=X_test)
 #Ahora vamos a predecir
 
 print("F1 SECORE METRICAS")
 print("Mi modelo:",f1_score(ytest=y_test, ypredichas=y_predichas_mimodelo))
 print("Modelo de SKLEARN:",f1_score(ytest=y_test, ypredichas=y_predichas_modelo_sklearn))
 
+
+
+# Ahora vamos a hacer el diagrama
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+
+
+plt.contourf(X1, X2, np.array(regresion.algoritmo_sin_librerias(X_test=np.array([X1.ravel(), X2.ravel()]).T, X=X_train, Y=y_train, w=w, b=b)).reshape(X1.shape),
+             alpha = 0.5, cmap = Colors(('red', 'green')))
+
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = Colors(('red', 'green'))(i), label = j, s=10)
+plt.title('Regresion Logistica ')
+plt.xlabel('Largo de la URL')
+plt.ylabel('Indexado en Google')
+plt.show()
