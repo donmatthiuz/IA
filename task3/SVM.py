b6b26d456b1c6553f77ac6f0b719c62b1e826987
cor22982 <cor22982@uvg.edu.gt>
2025-02-03 01:57:20 -0600

    feat: Fix of svm

--- task3/SVM.py
+++ task3/SVM.py
@@ -6,16 +6,16 @@ class SVM ():
     self.learning_rate = learning_rate
     self.epocas = epocas
     self.lambda_par = lambda_par
-    self.w = []
-    self.b = 0
+    self.w = None
+    self.b = None
 
   def delta_j_w_1(self, w):
     return self.learning_rate * (2 * self.lambda_par * w)
   
   def delta_j_w_2(self, w, x_i, y_i):
-    return self.learning_rate * ( 2 * self.lambda_par *  w - np.dot(x_i, y_i) )
+    return self.learning_rate * (2 * self.lambda_par * w - np.dot(x_i, y_i))
   
-  def algoritmo_sin_librerias_entrenamiento(self, X, Y, X_test):
+  def algoritmo_sin_librerias_fit(self, X, Y):
     _, n_s = X.shape
 
     y = np.where(Y <= 0, -1, 1)
@@ -28,19 +28,16 @@ class SVM ():
         if condition:
           self.w -= self.delta_j_w_1(self.w)
         else:
-          self.w -= self.delta_j_w_2(x_i, y[j], self.w, self.b)
-          self.b -=  y[j] * self.learning_rate
+          self.w -= self.delta_j_w_2(w=self.w, x_i=x_i, y_i=y[j])
+          self.b -=  self.learning_rate * y[j]  
        if i % 100 == 0:
-         print(f"Epoca actual {i}, Perdida: {condition}")
-    
-    y_predichas = np.dot(X_test, self.w) - self.b
-    return np.sign(y_predichas)
+         print(f"Epoca actual {i}")
   
+  def algoritmo_sin_librerias_prediccion(self, X_test):
+     y_predichas = np.dot(X_test, self.w) - self.b
+     return np.sign(y_predichas)
 
   def algoritmo_con_librerias(self, X_train, Y_train, X_test):
     clf = svm.SVC()
     clf.fit(X_train, Y_train)
     return clf.predict(X_test)
-
-
-  
--- task3/task3.py
+++ task3/task3.py
@@ -1,13 +1,82 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from SVM import SVM, SVM2
+
 
 # Replace the path with your actual file path
 csv_file_path = 'task3\\high_diamond_ranked_10min.csv'
 
+
+
+# ================== PARTE 3.1  ANALISIS EXPLORATORIO ===================
+
 # Load the CSV into a DataFrame
 df = pd.read_csv(csv_file_path)
 
-# Display the DataFrame
-print(df.columns)
+
+## Primero vamos a verificar que este balanceado
+ceros = df.loc[df['blueWins'] == 0]
+unos = df.loc[df['blueWins'] == 1]
+
+#Se puede ver que estan balanceados pues blueWins tiene 4949 y 4930 de ceros y unos
+print(ceros["blueWins"].count())
+print(unos["blueWins"].count())
+
+
+
+# Eleccion de variables 
+
+# Como variables vamos a ver cuales hay. 
+
+# Eligiremos el blueAvgLevel
+# Eligiremos el blueKills
+
+# Vamos a usar estas 2 debido a que se intuye que tener un nivel 
+# en el promedio de los azules y las muertes provocadas que puede 
+
+
+#Mostramos el rango de los valores
+print(f"Promedio de nivel de los Azules {df["blueAvgLevel"].min()} - {df["blueAvgLevel"].max()}")
+print(f"Muertes causadas por los Azules {df["blueKills"].min()} - {df["blueKills"].max()}")
+
+
+# No es necesario escalarlas ni mapearlas , tampoco normalizarlas. Por lo que se usara como estan
+
+#=====================PARTE 3.2 Support Vector Machines  Clasificación de Partidas de League of Legends==============
+
+#Vamos a dividir en 2 test y entrenamiento
+
+X = df[['blueAvgLevel','blueKills']].values
+Y = df['blueWins'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, random_state=42)
+
+# ENTRENAMIENTO 80%
+
+#Variables a Definir
+learning_rate = 0.001
+epocas = 1000
+lambda_par =0.01
+
+svm = SVM(learning_rate=learning_rate, epocas=epocas, lambda_par=lambda_par)
+
+svm.algoritmo_sin_librerias_fit(X=X_train, Y=y_train)
+
+y_manual_predichas = svm.algoritmo_sin_librerias_prediccion(X_test=X_test)
+
+#y_librerias_predichas = svm.algoritmo_con_librerias(X_train=X_train, Y_train=y_train, X_test= X_test)
+#print(y_manual_predichas)
+
+# svm2 = SVM2()
+
+# svm2.fit(X_train, y_train)
+# ys = svm2.predict(X_test)
+print(y_manual_predichas)
+
+
+
 
 
 
